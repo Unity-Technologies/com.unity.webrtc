cee30d32a96c3db9c06722e4158034d9a1cd0e50
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-10-11 18:37:26 +0900

    Add common macros: SAFE_RELEASE, SAFE_DELETE

--- Plugin~/WebRTCPlugin/GraphicsDevice/D3D11Texture2D.h
+++ Plugin~/WebRTCPlugin/GraphicsDevice/D3D11Texture2D.h
@@ -3,6 +3,8 @@
 #include "ITexture2D.h"
 #include "d3d11.h"
 
+#include "WebRTCMacros.h"
+
 namespace WebRTC {
 
 struct D3D11Texture2D : ITexture2D {
@@ -12,10 +14,7 @@ public:
     D3D11Texture2D(uint32_t w, uint32_t h,ID3D11Texture2D* tex);
 
     virtual ~D3D11Texture2D() {
-        if (nullptr != m_texture) {
-            m_texture->Release();
-            m_texture = nullptr;
-        }
+        SAFE_RELEASE(m_texture);
     }
 
     inline virtual void* GetNativeTexturePtrV();
