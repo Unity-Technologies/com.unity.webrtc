0dccae085c998dd4d39fd48eefd3d8e9857b9f40
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-10-11 17:42:13 +0900

    First implementation of GraphicsDevice.
    Only for initializing the device and shutting down

---
+++ Plugin~/WebRTCPlugin/GraphicsDevice/GraphicsDevice.cpp
@@ -0,0 +1,72 @@
+#include "pch.h"
+#include "GraphicsDevice.h"
+#include "WebRTCConstants.h" //NUM_TEXTURES_FOR_BUFFERING
+
+namespace WebRTC {
+
+ID3D11DeviceContext* context; ////d3d11 context
+ID3D11Device* g_D3D11Device = nullptr; ////d3d11 device
+ID3D11Texture2D* g_renderTextures[NUM_TEXTURES_FOR_BUFFERING]; ////natively created ID3D11Texture2D ptrs
+
+GraphicsDevice& GraphicsDevice::GetInstance() {
+    static GraphicsDevice device;
+    return device;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+void GraphicsDevice::Init(IUnityInterfaces* unityInterface) {
+
+    m_rendererType = unityInterface->Get<IUnityGraphics>()->GetRenderer();
+    if (m_rendererType == kUnityGfxRendererD3D11)
+    {
+        g_D3D11Device = unityInterface->Get<IUnityGraphicsD3D11>()->GetDevice();
+        g_D3D11Device->GetImmediateContext(&context);
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+void GraphicsDevice::Shutdown() {
+    for (auto rt : g_renderTextures)  {
+        if (rt) {
+            rt->Release();
+            rt = nullptr;
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//void* GraphicsDevice::CreateEncoderInputTexture(uint32_t w , uint32_t h ) {
+//
+//    //[TODO-sin: 2019-10-11] Move this to GraphicsDeviceDX11
+//    ID3D11Texture2D* texture = nullptr;
+//    D3D11_TEXTURE2D_DESC desc = { 0 };
+//    desc.Width = w;
+//    desc.Height = h;
+//    desc.MipLevels = 1;
+//    desc.ArraySize = 1;
+//    desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+//    desc.SampleDesc.Count = 1;
+//    desc.Usage = D3D11_USAGE_DEFAULT;
+//    desc.BindFlags = D3D11_BIND_RENDER_TARGET;
+//    desc.CPUAccessFlags = 0;
+//    HRESULT r = g_D3D11Device->CreateTexture2D(&desc, NULL, &texture);
+//    return texture;
+//}
+//
+////---------------------------------------------------------------------------------------------------------------------
+//
+//void GraphicsDevice::ReleaseEncoderInputTexture(void *texture) {
+//    ID3D11Texture2D* tex = reinterpret_cast<ID3D11Texture2D*>(texture);
+//    tex->Release();
+//}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+GraphicsDevice::GraphicsDevice() : m_rendererType(static_cast<UnityGfxRenderer>(0)) {
+
+}
+
+}
+
---
+++ Plugin~/WebRTCPlugin/GraphicsDevice/GraphicsDevice.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace WebRTC {
+
+//Singleton
+class GraphicsDevice {
+
+    public:
+        static GraphicsDevice& GetInstance();
+        void Init(IUnityInterfaces* unityInterface);
+        void Shutdown();
+        //void* CreateEncoderInputTexture(uint32_t w , uint32_t h );
+        //void ReleaseEncoderInputTexture(void *texture);
+
+    private:
+        GraphicsDevice();
+        GraphicsDevice(GraphicsDevice const&);              
+        void operator=(GraphicsDevice const&);
+
+        UnityGfxRenderer m_rendererType;
+
+};
+
+}
