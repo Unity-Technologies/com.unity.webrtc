5e90872110c4a2eb85f9ad577ade19eccc3618ef
kazuki matsumoto <karasusan@gmail.com>
2019-10-26 21:41:41 +0900

    Fixed build process to create a plugin for Linux

--- BuildScripts~/build_plugin.sh
+++ BuildScripts~/build_plugin.sh
@@ -8,7 +8,12 @@ export SOLUTION_DIR=$(pwd)/Plugin~
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
 
+# Install libc++ and libc++abi
+# TODO:: Remove this install process from here and recreate an image to build the plugin.
+sudo apt install -y libc++-dev libc++abi-dev
+
 # Build UnityRenderStreaming Plugin 
 
 cd $SOLUTION_DIR
 cmake .
+make
