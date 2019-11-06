20f52f1efd6b33c0a1cdfe6459dfe05ad91cf3ae
kazuki matsumoto <karasusan@gmail.com>
2019-11-06 18:06:19 +0900

    add script to install glew

--- BuildScripts~/build_plugin.sh
+++ BuildScripts~/build_plugin.sh
@@ -8,9 +8,9 @@ export SOLUTION_DIR=$(pwd)/Plugin~
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
 
-# Install libc++ and libc++abi
+# Install libc++, libc++abi and glew 
 # TODO:: Remove this install process from here and recreate an image to build the plugin.
-sudo apt install -y libc++-dev libc++abi-dev
+sudo apt install -y libc++-dev libc++abi-dev libglew-dev
 
 # Build UnityRenderStreaming Plugin 
 cd $SOLUTION_DIR
