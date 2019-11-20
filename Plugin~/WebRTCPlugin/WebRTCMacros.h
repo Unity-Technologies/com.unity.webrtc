1850817be2b146b197913a7739a15795b99d6f0f
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-11-20 19:37:17 +0900

    Add Vulkan related macros

--- Plugin~/WebRTCPlugin/WebRTCMacros.h
+++ Plugin~/WebRTCPlugin/WebRTCMacros.h
@@ -2,3 +2,24 @@
 
 #define SAFE_DELETE(a)      { if( (a) != NULL ) { delete     (a); (a) = nullptr; } }  
 #define SAFE_DELETE_ARR(a)  { if( (a) != NULL ) { delete[]   (a); (a) = nullptr; } }
+
+#define VULKAN_SAFE_DESTROY_IMAGE_VIEW(device, obj, allocator) { \
+    if (VK_NULL_HANDLE != obj) { \
+        vkDestroyImageView(device, obj, allocator); \
+        obj = VK_NULL_HANDLE; \
+    } \
+}
+
+#define VULKAN_SAFE_DESTROY_IMAGE(device, obj, allocator) { \
+    if (VK_NULL_HANDLE!=obj) { \
+        vkDestroyImage(device, obj, allocator); \
+        obj = VK_NULL_HANDLE; \
+    } \
+}
+
+#define VULKAN_SAFE_FREE_MEMORY(device, obj, allocator) { \
+    if (VK_NULL_HANDLE!=obj) { \
+        vkFreeMemory(device, obj, allocator); \
+        obj = VK_NULL_HANDLE; \
+    } \
+}
