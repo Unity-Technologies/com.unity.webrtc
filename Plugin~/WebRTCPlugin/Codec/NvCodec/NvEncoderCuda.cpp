d13e8b415f4b2fcd53aa42a941ff64a246ad84b7
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-11-21 14:59:03 +0900

    Add NvEncoderCuda

---
+++ Plugin~/WebRTCPlugin/Codec/NvCodec/NvEncoderCuda.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "nvEncodeAPI.h"
+#include "NvEncoderCuda.h"
+#include "GraphicsDevice/IGraphicsDevice.h"
+#include "GraphicsDevice/ITexture2D.h"
+
+namespace WebRTC {
+
+    NvEncoderCuda::NvEncoderCuda(const uint32_t nWidth, const uint32_t nHeight, IGraphicsDevice* device) :
+        NvEncoder(NV_ENC_DEVICE_TYPE_CUDA, NV_ENC_INPUT_RESOURCE_TYPE_CUDAARRAY, nWidth, nHeight, device)
+    {
+    }
+
+    void* NvEncoderCuda::AllocateInputResourceV(ITexture2D* tex) {
+        return tex->GetEncodeTexturePtrV();
+    }
+
+}
---
+++ Plugin~/WebRTCPlugin/Codec/NvCodec/NvEncoderCuda.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "NvEncoder.h"
+
+namespace WebRTC {
+    class NvEncoderCuda : public NvEncoder {
+    public:
+        NvEncoderCuda(uint32_t nWidth, uint32_t nHeight, IGraphicsDevice* device);
+        virtual ~NvEncoderCuda() = default;
+    protected:
+        virtual void* AllocateInputResourceV(ITexture2D* tex) override;
+    };
+}
