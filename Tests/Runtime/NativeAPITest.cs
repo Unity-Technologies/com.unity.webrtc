36140f8abaa150c04c47eeb7d623433f13f192ea
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-01-09 15:59:22 +0900

    Revert "Revert "Revert "Remove all OnRenderEvent"""
    This reverts commit 5f5a2791cff3817475efb68a15b79c4c56be4636.

--- Tests/Runtime/NativeAPITest.cs
+++ Tests/Runtime/NativeAPITest.cs
@@ -101,8 +101,6 @@ class NativeAPITest
         var stream = NativeMethods.ContextCreateVideoStream(context, renderTexture.GetNativeTexturePtr(), width, height);
         var callback = NativeMethods.GetRenderEventFunc(context);
 
-        Assert.IsFalse(callback == IntPtr.Zero);
-
         // TODO::
         // note:: You must call `InitializeEncoder` method after `NativeMethods.ContextCaptureVideoStream`
         VideoEncoderMethods.InitializeEncoder(callback);
