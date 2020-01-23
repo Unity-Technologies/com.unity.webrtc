f868fd8333688ae794b6f8e24bf88b752111ec07
kazuki matsumoto <karasusan@gmail.com>
2020-01-23 20:05:48 +0900

    Add audiostream test

--- Tests/Runtime/MediaStreamTest.cs
+++ Tests/Runtime/MediaStreamTest.cs
@@ -21,29 +21,42 @@ namespace Unity.WebRTC.RuntimeTest
         }
 
         [UnityTest]
-        public IEnumerator MediaStreamTest_AddAndRemoveTrack()
+        public IEnumerator MediaStreamTest_AddAndRemoveVideoStream()
         {
             var camObj = new GameObject("Camera");
             var cam = camObj.AddComponent<Camera>();
             var videoStream = cam.CaptureStream(1280, 720);
             yield return new WaitForSeconds(1.0f);
+            Assert.AreEqual(1, videoStream.GetVideoTracks().Length);
+            Assert.AreEqual(0, videoStream.GetAudioTracks().Length);
+            Assert.AreEqual(1, videoStream.GetTracks().Length);
             yield return videoStream.FinalizeEncoder();
             yield return new WaitForSeconds(1.0f);
             videoStream.Dispose();
+            Object.DestroyImmediate(camObj);
         }
+
+        [Test]
+        public void MediaStreamTest_AddAndRemoveAudioStream()
+        {
+            var audioStream = Audio.CaptureStream();
+            Assert.AreEqual(1, audioStream.GetAudioTracks().Length);
+            Assert.AreEqual(0, audioStream.GetVideoTracks().Length);
+            Assert.AreEqual(1, audioStream.GetTracks().Length);
+            audioStream.Dispose();
+        }
+
 
         [UnityTest]
         [Timeout(5000)]
-        public IEnumerator MediaStreamTest_AddAndRemoveMediaStream()
+        public IEnumerator MediaStreamTest_AddAndRemoveAudioTrack()
         {
-            var camObj = new GameObject("Camera");
-            var cam = camObj.AddComponent<Camera>();
             RTCConfiguration config = default;
             config.iceServers = new[]
             {
                 new RTCIceServer {urls = new[] {"stun:stun.l.google.com:19302"}}
             };
-
+            var audioStream = Audio.CaptureStream();
             var pc1Senders = new List<RTCRtpSender>();
             var pc2Senders = new List<RTCRtpSender>();
             var peer1 = new RTCPeerConnection(ref config);
@@ -52,16 +65,11 @@ namespace Unity.WebRTC.RuntimeTest
             peer1.OnIceCandidate = candidate => { peer2.AddIceCandidate(ref candidate); };
             peer2.OnIceCandidate = candidate => { peer1.AddIceCandidate(ref candidate); };
             peer2.OnTrack = e => { pc2Senders.Add(peer2.AddTrack(e.Track)); };
-            var videoStream = cam.CaptureStream(1280, 720);
-            yield return new WaitForSeconds(1.0f);
 
-            foreach (var track in videoStream.GetTracks())
+            foreach (var track in audioStream.GetTracks())
             {
                 pc1Senders.Add(peer1.AddTrack(track));
             }
-
-            var audioStream = Audio.CaptureStream();
-            peer1.AddTrack(audioStream.GetTracks()[0]);
 
             RTCOfferOptions options1 = default;
             RTCAnswerOptions options2 = default;
@@ -103,16 +111,88 @@ namespace Unity.WebRTC.RuntimeTest
             {
                 peer2.RemoveTrack(sender);
             }
-
             pc1Senders.Clear();
-            GameObject.DestroyImmediate(camObj);
+            peer1.Close();
+            peer2.Close();
+            audioStream.Dispose();
+        }
 
+        [UnityTest]
+        [Timeout(5000)]
+        public IEnumerator MediaStreamTest_AddAndRemoveVideoTrack()
+        {
+            var camObj = new GameObject("Camera");
+            var cam = camObj.AddComponent<Camera>();
+            RTCConfiguration config = default;
+            config.iceServers = new[]
+            {
+                new RTCIceServer {urls = new[] {"stun:stun.l.google.com:19302"}}
+            };
+
+            var pc1Senders = new List<RTCRtpSender>();
+            var pc2Senders = new List<RTCRtpSender>();
+            var peer1 = new RTCPeerConnection(ref config);
+            var peer2 = new RTCPeerConnection(ref config);
+
+            peer1.OnIceCandidate = candidate => { peer2.AddIceCandidate(ref candidate); };
+            peer2.OnIceCandidate = candidate => { peer1.AddIceCandidate(ref candidate); };
+            peer2.OnTrack = e => { pc2Senders.Add(peer2.AddTrack(e.Track)); };
+            var videoStream = cam.CaptureStream(1280, 720);
+            yield return new WaitForSeconds(1.0f);
+
+            foreach (var track in videoStream.GetTracks())
+            {
+                pc1Senders.Add(peer1.AddTrack(track));
+            }
+
+            RTCOfferOptions options1 = default;
+            RTCAnswerOptions options2 = default;
+            var op1 = peer1.CreateOffer(ref options1);
+            yield return op1;
+            var op2 = peer1.SetLocalDescription(ref op1.desc);
+            yield return op2;
+            var op3 = peer2.SetRemoteDescription(ref op1.desc);
+            yield return op3;
+            var op4 = peer2.CreateAnswer(ref options2);
+            yield return op4;
+            var op5 = peer2.SetLocalDescription(ref op4.desc);
+            yield return op5;
+            var op6 = peer1.SetRemoteDescription(ref op4.desc);
+            yield return op6;
+
+            var op7 = new WaitUntilWithTimeout(() =>
+                peer1.IceConnectionState == RTCIceConnectionState.Connected ||
+                peer1.IceConnectionState == RTCIceConnectionState.Completed, 5000);
+            yield return op7;
+            Assert.True(op7.IsCompleted);
+
+            var op8 = new WaitUntilWithTimeout(() =>
+                peer2.IceConnectionState == RTCIceConnectionState.Connected ||
+                peer2.IceConnectionState == RTCIceConnectionState.Completed, 5000);
+            yield return op8;
+            Assert.True(op7.IsCompleted);
+
+            var op9 = new WaitUntilWithTimeout(() => pc2Senders.Count > 0, 5000);
+            yield return op9;
+            Assert.True(op9.IsCompleted);
+
+            foreach (var sender in pc1Senders)
+            {
+                peer1.RemoveTrack(sender);
+            }
+
+            foreach (var sender in pc2Senders)
+            {
+                peer2.RemoveTrack(sender);
+            }
+            pc1Senders.Clear();
             peer1.Close();
             peer2.Close();
 
             yield return videoStream.FinalizeEncoder();
             yield return new WaitForSeconds(1.0f);
             videoStream.Dispose();
+            Object.DestroyImmediate(camObj);
         }
     }
 }
