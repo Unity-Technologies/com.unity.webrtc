eebf1dd5217774a48693b84278dfefd8ae26a50e
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-01-27 10:38:29 +0900

    Add a test to make sure the plugins have correct import settings (#57)

--- Tests/Editor/PluginTest.cs
+++ Tests/Editor/PluginTest.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;    // Process
 using System.Linq;
 using UnityEngine.TestTools; // UnityPlatform
 using NUnit.Framework;       // Assert
+using UnityEditor;
 
 namespace Unity.WebRTC.Editor {
 
@@ -27,6 +28,25 @@ public class PluginTest {
 
         Assert.Contains(WebRTC.GetModuleName(), names);
     }
+
+//----------------------------------------------------------------------------------------------------------------------
+    [Test]
+    public static void CheckPluginImportSettings() {
+
+        string[] guids = AssetDatabase.FindAssets("", new[] {"Packages/com.unity.webrtc/Runtime/Plugins"});
+        foreach (string guid in guids) {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            AssetImporter assetImporter = AssetImporter.GetAtPath(path);
+            Assert.IsNotNull(assetImporter);
+            if (assetImporter.GetType() != typeof(PluginImporter))
+                continue;
+            PluginImporter pluginImporter = assetImporter as PluginImporter;
+            Assert.IsNotNull(pluginImporter);
+            Assert.IsTrue(pluginImporter.isPreloaded);
+        }
+       
+    }
+
 }
 
 } //namespace Unity.WebRTC.Editor
