d7286f3257468de6cd6f60ff517c3019789ffb55
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-02-26 03:05:39 +0900

    revert build libwebrtc win

--- BuildScripts~/build_libwebrtc_win.cmd
+++ BuildScripts~/build_libwebrtc_win.cmd
@@ -17,19 +17,18 @@ cmd /k fetch.bat webrtc
 
 cd src
 cmd /k git.bat config --system core.longpaths true
-cmd /k git.bat branch -r
-cmd /k git.bat checkout -b my_branch refs/remotes/branch-heads/%WEBRTC_VERSION%
+cmd /k git.bat checkout  refs/remotes/branch-heads/%WEBRTC_VERSION%
 cd ..
 
 cmd /k gclient.bat sync -f
 
+REM add jsoncpp
+patch "src\BUILD.gn" < "BuildScripts~\add_jsoncpp.patch"
+
 REM install pywin32
 cmd /k %cd%\depot_tools\bootstrap-3_8_0_chromium_8_bin\python\bin\python.exe -m pip install pywin32
 
-REM change jsoncpp static library
-powershell -File ".\BuildScripts~\ReplaceText.ps1" "src\third_party\jsoncpp\BUILD.gn" "source_set" "static_library"
-
-cmd /k gn.bat gen %OUTPUT_DIR% --root="src" --args="is_debug=false is_clang=false target_cpu=\"x64\" symbol_level=0 enable_iterator_debugging=false"
+cmd /k gn.bat gen %OUTPUT_DIR% --root="src" --args="is_debug=false is_clang=false target_cpu=\"x64\" rtc_include_tests=false rtc_build_examples=false rtc_use_h264=false symbol_level=0 enable_iterator_debugging=false"
 
 REM add json.obj in link list of webrtc.ninja
 powershell -File ".\BuildScripts~\ReplaceText.ps1" "%OUTPUT_DIR%\obj\webrtc.ninja" "obj/rtc_base/rtc_base/crc32.obj" "obj/rtc_base/rtc_base/crc32.obj obj/rtc_base/rtc_json/json.obj"
@@ -52,7 +51,7 @@ xcopy src\*.h %ARTIFACTS_DIR%\include /C /S /I /F /H
 
 REM copy lib
 mkdir %ARTIFACTS_DIR%\lib
-for %%G in (webrtc.lib audio_decoder_opus.lib webrtc_opus.lib jsoncpp.lib) do forfiles /P "%cd%\%OUTPUT_DIR%" /S /M %%G /C "cmd /c copy @path %ARTIFACTS_DIR%\lib"
+for %%G in (webrtc.lib audio_decoder_opus.lib webrtc_opus.lib) do forfiles /P "%cd%\%OUTPUT_DIR%" /S /M %%G /C "cmd /c copy @path %ARTIFACTS_DIR%\lib"
 
 REM copy license
 copy %OUTPUT_DIR%\LICENSE.md %ARTIFACTS_DIR%
