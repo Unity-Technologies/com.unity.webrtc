5da6b03f53fbaa3be9614b75916ee427990130b1
kazuki <karasusan@gmail.com>
2020-03-19 18:42:42 +0900

    remove comment

--- Plugin~/WebRTCPlugin/Codec/EncoderFactory.h
+++ Plugin~/WebRTCPlugin/Codec/EncoderFactory.h
@@ -9,14 +9,10 @@ namespace WebRTC {
     public:
         static EncoderFactory& GetInstance();
         static bool GetHardwareEncoderSupport();
-        //bool IsInitialized() const;
         std::unique_ptr<IEncoder> Init(int width, int height, IGraphicsDevice* device, UnityEncoderType encoderType); //Can throw exception.
-        //void Shutdown();
-        //IEncoder *GetEncoder() const;
     private:
         EncoderFactory() = default;
         EncoderFactory(EncoderFactory const&) = delete;
         EncoderFactory& operator=(EncoderFactory const&) = delete;
-        //std::unique_ptr<IEncoder> m_encoder = nullptr;
     };
 }
