c8cc54aeed7b91ffc5cc26a30789e5f804ba89e3
kazuki <karasusan@gmail.com>
2020-03-26 16:23:52 +0900

    test add audio test

---
+++ Tests/Runtime/AudioTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using UnityEngine.PlayerLoop;
+
+namespace Unity.WebRTC.RuntimeTest
+{
+    class AudioTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            WebRTC.Initialize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            WebRTC.Dispose();
+        }
+
+        [Test]
+        public void Update()
+        {
+            var stream = Audio.CaptureStream();
+            float[] audioData = new float[128];
+            Audio.Update(audioData, 1);
+            Audio.Stop();
+            stream.Dispose();
+        }
+    }
+}
