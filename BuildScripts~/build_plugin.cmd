fa041597ca080f62e36b69554e7a33edfed77381
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-05-14 13:41:16 +0900

    change build plugin download url m83

--- BuildScripts~/build_plugin.cmd
+++ BuildScripts~/build_plugin.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M79/webrtc-win.zip
+set LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M83/webrtc-win.zip
 set SOLUTION_DIR=%cd%\Plugin~
 
 echo -------------------
