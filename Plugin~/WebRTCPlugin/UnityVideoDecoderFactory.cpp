957369d90a0c104e65a2ae167e573211d10f5326
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-06-02 18:33:51 +0900

    update tab to space

--- Plugin~/WebRTCPlugin/UnityVideoDecoderFactory.cpp
+++ Plugin~/WebRTCPlugin/UnityVideoDecoderFactory.cpp
@@ -8,10 +8,11 @@ namespace webrtc
     UnityVideoDecoderFactory::UnityVideoDecoderFactory(): internal_decoder_factory_(new webrtc::InternalDecoderFactory())
     {
     }
-	std::vector<webrtc::SdpVideoFormat> UnityVideoDecoderFactory::GetSupportedFormats() const
-	{
+
+    std::vector<webrtc::SdpVideoFormat> UnityVideoDecoderFactory::GetSupportedFormats() const
+    {
         return { webrtc::CreateH264Format(webrtc::H264::kProfileConstrainedBaseline, webrtc::H264::kLevel5_1, "1") };
-	}
+    }
 
     std::unique_ptr<webrtc::VideoDecoder> UnityVideoDecoderFactory::CreateVideoDecoder(const webrtc::SdpVideoFormat & format)
     {
