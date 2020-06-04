36a63a2788928fc0c6d846746e23f1171ecfaa52
Takashi Kannan <26959415+kannan-xiao4@users.noreply.github.com>
2020-06-04 17:33:57 +0900

    Update libwebrtc m83 (#121)
    * update libwebrtc branch m83
    
    * change build plugin download url m83
    
    * fix compile error
    
    * add error struct on callback
    
    * change build libwebrtc image
    
    * add unity videodecodefactory(only use for test)
    
    * update webrtc plugin
    
    * update tab to space

--- Plugin~/WebRTCPlugin/Context.cpp
+++ Plugin~/WebRTCPlugin/Context.cpp
@@ -8,6 +8,7 @@
 #include "MediaStreamObserver.h"
 #include "SetSessionDescriptionObserver.h"
 #include "UnityVideoEncoderFactory.h"
+#include "UnityVideoDecoderFactory.h"
 
 namespace unity
 {
@@ -149,10 +150,14 @@ namespace webrtc
 #if defined(SUPPORT_METAL) && defined(SUPPORT_SOFTWARE_ENCODER)
         //Always use SoftwareEncoder on Mac for now.
         std::unique_ptr<webrtc::VideoEncoderFactory> videoEncoderFactory = webrtc::CreateBuiltinVideoEncoderFactory();
+        std::unique_ptr<webrtc::VideoDecoderFactory> videoDecoderFactory = webrtc::CreateBuiltinVideoDecoderFactory();
 #else
         std::unique_ptr<webrtc::VideoEncoderFactory> videoEncoderFactory =
             m_encoderType == UnityEncoderType::UnityEncoderHardware ?
             std::make_unique<UnityVideoEncoderFactory>(static_cast<IVideoEncoderObserver*>(this)) : webrtc::CreateBuiltinVideoEncoderFactory();
+        std::unique_ptr<webrtc::VideoDecoderFactory> videoDecoderFactory =
+            m_encoderType == UnityEncoderType::UnityEncoderHardware ?
+            std::make_unique<UnityVideoDecoderFactory>() : webrtc::CreateBuiltinVideoDecoderFactory();
 #endif
 
         m_peerConnectionFactory = webrtc::CreatePeerConnectionFactory(
@@ -163,7 +168,7 @@ namespace webrtc
                                 webrtc::CreateAudioEncoderFactory<webrtc::AudioEncoderOpus>(),
                                 webrtc::CreateAudioDecoderFactory<webrtc::AudioDecoderOpus>(),
                                 std::move(videoEncoderFactory),
-                                webrtc::CreateBuiltinVideoDecoderFactory(),
+                                std::move(videoDecoderFactory),
                                 nullptr,
                                 nullptr);
     }
