949d165eb0a6b4a93d1f6a4c5e2b86bda2ca17e2
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-06-23 12:18:37 +0900

    wip dump implement
    (cherry picked from commit 9b227621dabb116e1b52c80338620008c5ef3f73)

--- Editor/WebRTCInternals.cs
+++ Editor/WebRTCInternals.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.EditorCoroutines.Editor;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UIElements;
 
 namespace Unity.WebRTC.Editor
 {
     public delegate void OnPeerListHandler(IEnumerable<RTCPeerConnection> peerList);
+
     public delegate void OnStatsReportHandler(RTCPeerConnection peer, RTCStatsReport statsReport);
 
     public class WebRTCInternals : EditorWindow
@@ -27,10 +30,20 @@ namespace Unity.WebRTC.Editor
 
         private EditorCoroutine m_editorCoroutine;
 
+        private Dictionary<int, PeerConnectionRecord> m_peerConenctionDataStore =
+            new Dictionary<int, PeerConnectionRecord>();
+
         private void OnEnable()
         {
             var root = this.rootVisualElement;
             root.Add(CreateStatsView());
+            root.Add(new Button(() =>
+            {
+                foreach (var record in m_peerConenctionDataStore)
+                {
+                    Debug.Log(record.Value.ToJson());
+                }
+            }){text = "jsontest"});
 
             EditorApplication.playModeStateChanged += change =>
             {
@@ -40,11 +53,11 @@ namespace Unity.WebRTC.Editor
                         m_editorCoroutine = EditorCoroutineUtility.StartCoroutineOwnerless(GetStatsPolling());
                         break;
                     case PlayModeStateChange.ExitingPlayMode:
+                        m_peerConenctionDataStore.Clear();
                         EditorCoroutineUtility.StopCoroutine(m_editorCoroutine);
                         break;
                 }
             };
-
         }
 
         private void OnDisable()
@@ -70,6 +83,14 @@ namespace Unity.WebRTC.Editor
                         if (!op.IsError)
                         {
                             OnStats?.Invoke(peer, op.Value);
+
+                            var peerId = peer.GetHashCode();
+                            if (!m_peerConenctionDataStore.ContainsKey(peerId))
+                            {
+                                m_peerConenctionDataStore[peerId] = new PeerConnectionRecord();
+                            }
+
+                            m_peerConenctionDataStore[peerId].Update(op.Value);
                         }
                     }
                 }
@@ -106,6 +127,67 @@ namespace Unity.WebRTC.Editor
             };
 
             return container;
+        }
+    }
+
+    public class PeerConnectionRecord
+    {
+        private const int MAX_BUFFER_SIZE = 1000;
+        private List<RTCStatsReport> m_statsReportList;
+
+        public PeerConnectionRecord()
+        {
+            m_statsReportList = new List<RTCStatsReport>();
+        }
+
+        public void Update(RTCStatsReport report)
+        {
+            if (m_statsReportList.Count > MAX_BUFFER_SIZE)
+            {
+                m_statsReportList.RemoveAt(0);
+            }
+
+            m_statsReportList.Add(report);
+        }
+
+        public string ToJson()
+        {
+            var map = new Dictionary<(RTCStatsType, string), List<RTCStats>>();
+            foreach (var element in m_statsReportList.SelectMany(report => report.Stats))
+            {
+                if (!map.ContainsKey(element.Key))
+                {
+                    map[element.Key] = new List<RTCStats>();
+                }
+
+                map[element.Key].Add(element.Value);
+            }
+
+            var hoge = map.SelectMany(x =>
+            {
+                var prefix = $"{x.Key.Item1}_{x.Key.Item2}";
+                var sorted = x.Value.OrderByDescending(y => y.Timestamp);
+                var start = sorted.First().Timestamp;
+                var end = sorted.Last().Timestamp;
+
+                var memberMap = new Dictionary<string, List<object>>();
+
+                foreach (var element in sorted.SelectMany(y => y.Dict))
+                {
+                    if (!memberMap.ContainsKey(element.Key))
+                    {
+                        memberMap[element.Key] = new List<object>();
+                    }
+
+                    memberMap[element.Key].Add(element.Value);
+                }
+
+                return memberMap.Select(y => $"{prefix}-{y.Key}:{{\"startTime\":\"{start}\", \"endTime\":\"{end}\", \"values\":\"[{string.Join(",", y.Value)}]\"}}");
+            });
+
+
+
+            return $"\"stats\":{{{string.Join(",", hoge)}}}";
         }
     }
 }
