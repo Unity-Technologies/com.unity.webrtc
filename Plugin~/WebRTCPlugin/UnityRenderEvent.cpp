c80a3d38ff1f1d810685298a715370acf11a8495
kazuki matsumoto <karasusan@gmail.com>
2020-07-28 16:26:08 +0900

    profile the encode operation (this feature is work well only Unity 2020)

--- Plugin~/WebRTCPlugin/UnityRenderEvent.cpp
+++ Plugin~/WebRTCPlugin/UnityRenderEvent.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
 
+#include <IUnityGraphics.h>
+#include <IUnityProfiler.h>
+
 #include "Codec/EncoderFactory.h"
 #include "Context.h"
-#include "IUnityGraphics.h"
 #include "GraphicsDevice/GraphicsDevice.h"
 
 enum class VideoStreamRenderEventID
@@ -16,13 +18,15 @@ namespace unity
 {
 namespace webrtc
 {
-
     IUnityInterfaces* s_UnityInterfaces = nullptr;
     IUnityGraphics* s_Graphics = nullptr;
+    IUnityProfiler* s_UnityProfiler = nullptr;
     Context* s_context = nullptr;
     IGraphicsDevice* s_device;
     std::map<const ::webrtc::MediaStreamTrackInterface*, std::unique_ptr<IEncoder>> s_mapEncoder;
 
+    const UnityProfilerMarkerDesc* s_MarkerEncode = nullptr;
+    bool s_IsDevelopmentBuild = false;
 } // end namespace webrtc
 } // end namespace unity
 
@@ -60,6 +64,14 @@ extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnit
     s_UnityInterfaces = unityInterfaces;
     s_Graphics = unityInterfaces->Get<IUnityGraphics>();
     s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
+
+    s_UnityProfiler = unityInterfaces->Get<IUnityProfiler>();
+    if (s_UnityProfiler != nullptr)
+    {
+        s_IsDevelopmentBuild = s_UnityProfiler->IsAvailable() != 0;
+        s_UnityProfiler->CreateMarker(&s_MarkerEncode, "Encode", kUnityProfilerCategoryRender, kUnityProfilerMarkerFlagDefault, 0);
+    }
+
     OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
 }
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
@@ -96,10 +108,15 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID, void* data)
         }
         case VideoStreamRenderEventID::Encode:
         {
+            if (s_IsDevelopmentBuild)
+                s_UnityProfiler->BeginSample(s_MarkerEncode);
             if(!s_context->EncodeFrame(track))
             {
                 LogPrint("Encode frame failed");
             }
+            if (s_IsDevelopmentBuild)
+                s_UnityProfiler->EndSample(s_MarkerEncode);
+
             return;
         }
         case VideoStreamRenderEventID::Finalize:
--- Plugin~/unity/include/IUnityProfilerCallbacks.h
+++ Plugin~/unity/include/IUnityProfilerCallbacks.h
@@ -6,13 +6,14 @@
 
 #pragma once
 #include "IUnityInterface.h"
+#include "IUnityProfiler.h"
 
 #include <stdint.h>
 
-// Unity Profiler Native plugin API provides an ability to register callbacks for Unity Profiler events.
+// Unity Profiler Callbacks native plugin API provides an ability to register callbacks for Unity Profiler events.
 // The basic functionality includes:
-// 1. Category callback - provides information about category name and color.
-// 2. Profiler marker callback - provides information about new marker which is created internally or with C# API.
+// 1. Profiler category creation callback - provides information about category name and color.
+// 2. Profiler marker creation callback - provides information about new marker which is created internally or with C# API.
 // 3. Marker event callback - allows to intercept begin/end events for the particular marker.
 // 4. Frame event callback - provides information about logical CPU frame.
 //
@@ -40,123 +41,27 @@
 //      }
 //  }
 //
-//  static void UNITY_INTERFACE_API MyProfilerCreateEventCallback(const UnityProfilerMarkerDesc* markerDesc, void* userData)
+//  static void UNITY_INTERFACE_API MyProfilerCreateMarkerCallback(const UnityProfilerMarkerDesc* markerDesc, void* userData)
 //  {
-//      s_UnityProfilerCallbacks->RegisterEventCallback(markerDesc, MyProfilerEventCallback, NULL);
+//      s_UnityProfilerCallbacks->RegisterMarkerEventCallback(markerDesc, MyProfilerEventCallback, NULL);
 //  }
 //
 //  extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
 //  {
 //      s_UnityProfilerCallbacks = unityInterfaces->Get<IUnityProfilerCallbacks>();
-//      s_UnityProfilerCallbacks->RegisterCreateEventCallback(&MyProfilerCreateEventCallback, NULL);
+//      s_UnityProfilerCallbacks->RegisterCreateMarkerCallback(&MyProfilerCreateMarkerCallback, NULL);
 //  }
 //
 //  extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
 //  {
-//      s_UnityProfilerCallbacks->UnregisterCreateEventCallback(&MyProfilerCreateEventCallback, NULL);
-//      s_UnityProfilerCallbacks->UnregisterEventCallback(NULL, &MyProfilerEventCallback, NULL);
+//      s_UnityProfilerCallbacks->UnregisterCreateMarkerCallback(&MyProfilerCreateMarkerCallback, NULL);
+//      s_UnityProfilerCallbacks->UnregisterMarkerEventCallback(NULL, &MyProfilerEventCallback, NULL);
 //  }
-
-
-typedef uint32_t UnityProfilerMarkerId;
-typedef uint16_t UnityProfilerCategoryId;
-typedef uint64_t UnityProfilerThreadId;
-
-typedef struct UnityProfilerCategoryDesc
-{
-    // Incremental category index.
-    UnityProfilerCategoryId id;
-    // Reserved.
-    uint16_t reserved0;
-    // Internally associated category color which is in 0xRRGGBBAA format.
-    uint32_t rgbaColor;
-    // NULL-terminated string which is associated with the category.
-    const char* name;
-} UnityProfilerCategoryDesc;
-
-enum UnityProfilerMarkerFlag_
-{
-    kUnityProfilerMarkerFlagDefault = 0,
-
-    kUnityProfilerMarkerFlagScriptUser = 1 << 1,         // Markers created with C# API.
-    kUnityProfilerMarkerFlagScriptInvoke = 1 << 5,       // Runtime invocations with ScriptingInvocation::Invoke.
-    kUnityProfilerMarkerFlagScriptEnterLeave = 1 << 6,   // Deep profiler.
-
-    kUnityProfilerMarkerFlagAvailabilityEditor = 1 << 2, // Editor-only marker, doesn't present in dev and non-dev players.
-    kUnityProfilerMarkerFlagAvailabilityNonDev = 1 << 3, // Non-dev marker, present everywhere.
-
-    kUnityProfilerMarkerFlagWarning = 1 << 4,            // Indicates undesirable, performance-wise suboptimal code path.
-
-    kUnityProfilerMarkerFlagVerbosityDebug    = 1 << 10, // Internal debug markers - e.g. JobSystem Idle.
-    kUnityProfilerMarkerFlagVerbosityInternal = 1 << 11, // Internal markers - e.g. Mutex/semaphore waits.
-    kUnityProfilerMarkerFlagVerbosityAdvanced = 1 << 12  // Markers which are useful for advanced users - e.g. Loading.
-};
-typedef uint16_t UnityProfilerMarkerFlags;
-
-enum UnityProfilerMarkerEventType_
-{
-    kUnityProfilerMarkerEventTypeBegin  = 0,
-    kUnityProfilerMarkerEventTypeEnd    = 1,
-    kUnityProfilerMarkerEventTypeSingle = 2
-};
-typedef uint16_t UnityProfilerMarkerEventType;
-
-typedef struct UnityProfilerMarkerDesc
-{
-    // Per-marker callback chain pointer. Don't use.
-    const void* callback;
-    // Event id.
-    UnityProfilerMarkerId id;
-    // UnityProfilerMarkerFlag_ value.
-    UnityProfilerMarkerFlags flags;
-    // Category index the marker belongs to.
-    UnityProfilerCategoryId categoryId;
-    // NULL-terminated string which is associated with the marker.
-    const char* name;
-    // Metadata descriptions chain. Don't use.
-    const void* metaDataDesc;
-} UnityProfilerMarkerDesc;
-
-enum UnityProfilerMarkerDataType_
-{
-    kUnityProfilerMarkerDataTypeNone = 0,
-    kUnityProfilerMarkerDataTypeInstanceId = 1,
-    kUnityProfilerMarkerDataTypeInt32 = 2,
-    kUnityProfilerMarkerDataTypeUInt32 = 3,
-    kUnityProfilerMarkerDataTypeInt64 = 4,
-    kUnityProfilerMarkerDataTypeUInt64 = 5,
-    kUnityProfilerMarkerDataTypeFloat = 6,
-    kUnityProfilerMarkerDataTypeDouble = 7,
-    kUnityProfilerMarkerDataTypeString = 8,
-    kUnityProfilerMarkerDataTypeString16 = 9,
-    kUnityProfilerMarkerDataTypeBlob8 = 11
-};
-typedef uint8_t UnityProfilerMarkerDataType;
-
-typedef struct UnityProfilerMarkerData
-{
-    UnityProfilerMarkerDataType type;
-    uint8_t reserved0;
-    uint16_t reserved1;
-    uint32_t size;
-    const void* ptr;
-} UnityProfilerMarkerData;
-
-enum UnityProfilerFlowEventType_
-{
-    // The flow began withing a current marker scope (enclosed scope).
-    kUnityProfilerFlowEventTypeBegin = 0,
-    // The flow continues with the next sample.
-    kUnityProfilerFlowEventTypeNext  = 1,
-    // The flow ends with the next sample.
-    kUnityProfilerFlowEventTypeEnd   = 2,
-};
-typedef uint8_t UnityProfilerFlowEventType;
 
 typedef struct UnityProfilerThreadDesc
 {
     // Thread id c-casted to uint64_t.
-    uint64_t threadId;
+    UnityProfilerThreadId threadId;
     // Thread group name. NULL-terminated.
     const char* groupName;
     // Thread name. NULL-terminated.
@@ -179,7 +84,9 @@ typedef void (UNITY_INTERFACE_API * IUnityProfilerCreateMarkerCallback)(const Un
 // \param eventDataCount is an event metadata count.
 // \param eventData is a metadata array of eventDataCount elements.
 // \param userData is an optional context pointer passed with RegisterCreateEventCallback call.
-typedef void (UNITY_INTERFACE_API * IUnityProfilerMarkerEventCallback)(const UnityProfilerMarkerDesc* markerDesc, UnityProfilerMarkerEventType eventType, uint16_t eventDataCount, const UnityProfilerMarkerData* eventData, void* userData);
+typedef void (UNITY_INTERFACE_API* IUnityProfilerMarkerEventCallback)(const UnityProfilerMarkerDesc* markerDesc, UnityProfilerMarkerEventType eventType, uint16_t eventDataCount, const UnityProfilerMarkerData* eventData, void* userData);
+
+typedef void (UNITY_INTERFACE_API * IUnityProfilerBulkCounterDataEventCallback)(int counterGroup, size_t size, void* data, void* userData);
 
 // Called when a profiler frame change occurs. Up to 4 installed callbacks are supported.
 // \param userData is an optional context pointer passed with RegisterCreateEventCallback call.
