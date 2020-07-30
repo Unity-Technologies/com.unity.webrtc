2eaae775bd38e1038575114781a65a88871594b9
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-07-30 14:11:02 +0900

    fix lint

--- Plugin~/WebRTCPlugin/UnityVideoDecoderFactory.h
+++ Plugin~/WebRTCPlugin/UnityVideoDecoderFactory.h
@@ -2,19 +2,19 @@
 
 namespace unity
 {
-    namespace webrtc
-    {
-        namespace webrtc = ::webrtc;
+namespace webrtc
+{
+    namespace webrtc = ::webrtc;
 
-        // This class is only used for status testing.
-        class UnityVideoDecoderFactory : public webrtc::VideoDecoderFactory
-        {
-        public:
-            std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override;
-            std::unique_ptr<webrtc::VideoDecoder> CreateVideoDecoder(const webrtc::SdpVideoFormat& format) override;
-            UnityVideoDecoderFactory();
-        private:
-            const std::unique_ptr<VideoDecoderFactory> internal_decoder_factory_;
-        };
-    }
+    // This class is only used for status testing.
+    class UnityVideoDecoderFactory : public webrtc::VideoDecoderFactory
+    {
+    public:
+        std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override;
+        std::unique_ptr<webrtc::VideoDecoder> CreateVideoDecoder(const webrtc::SdpVideoFormat& format) override;
+        UnityVideoDecoderFactory();
+    private:
+        const std::unique_ptr<VideoDecoderFactory> internal_decoder_factory_;
+    };
+}
 }
