5e823687f8ea2ed1418e8320d4ab75dfc5f50d73
kazuki matsumoto <karasusan@gmail.com>
2020-08-15 22:11:09 +0900

    add commit

--- Runtime/Scripts/WebRTC.cs
+++ Runtime/Scripts/WebRTC.cs
@@ -50,23 +50,6 @@ namespace Unity.WebRTC
         }
     }
 
-    public class RTCRtpReceiver
-    {
-        internal IntPtr self;
-        private RTCPeerConnection peer;
-
-        internal RTCRtpReceiver(IntPtr ptr, RTCPeerConnection peer)
-        {
-            self = ptr;
-            this.peer = peer;
-        }
-
-        public RTCStatsReportAsyncOperation GetStats()
-        {
-            return peer.GetStats(this);
-        }
-    }
-
     public enum RTCErrorDetailType
     {
         DataChannelFailure,
