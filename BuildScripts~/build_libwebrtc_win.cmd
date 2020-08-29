86d9814ac4378fdb3ff86aa03f0f03b64297f4a8
kazuki matsumoto <karasusan@gmail.com>
2020-08-29 10:45:03 +0900

    wip

--- BuildScripts~/build_libwebrtc_win.cmd
+++ BuildScripts~/build_libwebrtc_win.cmd
@@ -26,40 +26,43 @@ if not exist src (
 
 call gclient.bat sync -f
 
-REM add jsoncpp
-patch "src\BUILD.gn" < "%COMMAND_DIR%\add_jsoncpp.patch"
+rem add jsoncpp
+patch -N "src\BUILD.gn" < "%COMMAND_DIR%\add_jsoncpp.patch"
 
-REM install pywin32
-call %cd%\depot_tools\bootstrap-3_8_0_chromium_8_bin\python\bin\python.exe -m pip install pywin32 --upgrade
+rem install pywin32
+call "%cd%\depot_tools\bootstrap-3_8_0_chromium_8_bin\python\bin\python.exe" ^
+  -m pip install pywin32 --index-url "%PYPI_URL%" --upgrade
 
-call gn.bat gen %OUTPUT_DIR% --root="src" --args="is_debug=false is_clang=false target_cpu=\"x64\" rtc_include_tests=false rtc_build_examples=false rtc_use_h264=false symbol_level=0 enable_iterator_debugging=false"
+rem generate ninja
+call gn.bat gen %OUTPUT_DIR% --root="src" ^
+  --args="is_debug=false is_clang=false target_cpu=\"x64\" rtc_include_tests=false rtc_build_examples=false rtc_use_h264=false symbol_level=0 enable_iterator_debugging=false"
 
-REM add json.obj in link list of webrtc.ninja
-powershell -File "%COMMAND_DIR%\ReplaceText.ps1" "%OUTPUT_DIR%\obj\webrtc.ninja" "obj/rtc_base/rtc_base/crc32.obj" "obj/rtc_base/rtc_base/crc32.obj obj/rtc_base/rtc_json/json.obj"
-type "%OUTPUT_DIR%\obj\webrtc.ninja"
+rem update LIB_TO_LICENSES_DICT in generate_licenses.py
+powershell -File "%COMMAND_DIR%\ReplaceText.ps1" ^
+  "src\tools_webrtc\libs\generate_licenses.py" ^
+  "'ow2_asm': []," ^
+  "'ow2_asm': [], 'winsdk_samples': [], 'googletest': ['third_party/googletest/src/LICENSE'], 'nasm': ['third_party/nasm/LICENSE'], "
 
-REM update LIB_TO_LICENSES_DICT in generate_licenses.py
-powershell -File "%COMMAND_DIR%\ReplaceText.ps1" "src\tools_webrtc\libs\generate_licenses.py" "'ow2_asm': []," "'ow2_asm': [], 'winsdk_samples': [], 'googletest': ['third_party/googletest/src/LICENSE'], 'nasm': ['third_party/nasm/LICENSE'], "
-type "src\tools_webrtc\libs\generate_licenses.py"
-
+rem build
 ninja.exe -C %OUTPUT_DIR%
 
-REM generate license
-call python.bat .\src\tools_webrtc\libs\generate_licenses.py --target //:default %OUTPUT_DIR% %OUTPUT_DIR%
+rem generate license
+call python.bat .\src\tools_webrtc\libs\generate_licenses.py ^
+  --target //:default %OUTPUT_DIR% %OUTPUT_DIR%
 
-REM unescape license
-powershell -File "%COMMAND_DIR%\Unescape.ps1" %OUTPUT_DIR%\LICENSE.md			
+rem unescape license
+powershell -File "%COMMAND_DIR%\Unescape.ps1" "%OUTPUT_DIR%\LICENSE.md"
 
-REM copy header
-xcopy src\*.h %ARTIFACTS_DIR%\include /C /S /I /F /H
+rem copy header
+xcopy src\*.h "%ARTIFACTS_DIR%\include" /C /S /I /F /H
 
-REM copy lib
-mkdir %ARTIFACTS_DIR%\lib
-for %%G in (webrtc.lib audio_decoder_opus.lib webrtc_opus.lib) do forfiles /P "%cd%\%OUTPUT_DIR%" /S /M %%G /C "cmd /c copy @path %ARTIFACTS_DIR%\lib"
+rem copy lib
+mkdir "%ARTIFACTS_DIR%\lib"
+copy "%OUTPUT_DIR%\obj\webrtc.lib" "%ARTIFACTS_DIR%\lib"
 
-REM copy license
-copy %OUTPUT_DIR%\LICENSE.md %ARTIFACTS_DIR%
+rem copy license
+copy "%OUTPUT_DIR%\LICENSE.md" "%ARTIFACTS_DIR%"
 
-REM create zip
+rem create zip
 cd %ARTIFACTS_DIR%
 7z a -tzip webrtc-win.zip *
