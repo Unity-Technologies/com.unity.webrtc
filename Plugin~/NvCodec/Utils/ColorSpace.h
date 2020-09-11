f3f9122f857562239ac8f181d1ec30061f91cb30
kazuki matsumoto <karasusan@gmail.com>
2020-09-11 21:02:07 +0900

    add nvcodec sdk 9.1

---
+++ Plugin~/NvCodec/Utils/ColorSpace.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <stdint.h>
+#include <cuda_runtime.h>
+
+typedef enum ColorSpaceStandard {
+    ColorSpaceStandard_BT709 = 0,
+    ColorSpaceStandard_BT601 = 2,
+    ColorSpaceStandard_BT2020 = 4
+} ColorSpaceStandard;
+
+union BGRA32 {
+    uint32_t d;
+    uchar4 v;
+    struct {
+        uint8_t b, g, r, a;
+    } c;
+};
+
+union RGBA32 {
+    uint32_t d;
+    uchar4 v;
+    struct {
+        uint8_t r, g, b, a;
+    } c;
+};
+
+union BGRA64 {
+    uint64_t d;
+    ushort4 v;
+    struct {
+        uint16_t b, g, r, a;
+    } c;
+};
+
+union RGBA64 {
+    uint64_t d;
+    ushort4 v;
+    struct {
+        uint16_t r, g, b, a;
+    } c;
+};
