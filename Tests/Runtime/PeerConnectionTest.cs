12b613aca05fd37e7398f73208f30d380b90738c
kazuki matsumoto <karasusan@gmail.com>
2020-09-23 14:07:41 +0900

    fixed test error

--- Tests/Runtime/PeerConnectionTest.cs
+++ Tests/Runtime/PeerConnectionTest.cs
@@ -329,7 +329,6 @@ namespace Unity.WebRTC.RuntimeTest
                 foreach (var pair in stats.Dict)
                 {
                     Assert.IsNotEmpty(pair.Key);
-                    Assert.NotNull(pair.Value);
                 }
                 StatsCheck.Test(stats);
             }
