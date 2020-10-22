1f77fa699e6d4ed9cf67dd874946973ea557ccb3
Kazuki Matsumoto <karasusan@gmail.com>
2020-10-22 12:36:47 +0900

    fix test code to pass the runtime test on the virtual machine like parallels desktop

--- Tests/Runtime/AudioTest.cs
+++ Tests/Runtime/AudioTest.cs
@@ -7,7 +7,8 @@ namespace Unity.WebRTC.RuntimeTest
         [SetUp]
         public void SetUp()
         {
-            WebRTC.Initialize();
+            var value = NativeMethods.GetHardwareEncoderSupport();
+            WebRTC.Initialize(value ? EncoderType.Hardware : EncoderType.Software);
         }
 
         [TearDown]
