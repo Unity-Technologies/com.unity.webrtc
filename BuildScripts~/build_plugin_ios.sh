74b8ecb1dcbc04fd2316838489c05eb823ff3a1d
kazuki matsumoto <karasusan@gmail.com>
2020-11-25 17:04:36 +0900

    [skip ci] fix bugs

--- BuildScripts~/build_plugin_ios.sh
+++ BuildScripts~/build_plugin_ios.sh
@@ -2,6 +2,7 @@
 
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M85/webrtc-ios.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
+export WEBRTC_FRAMEWORK_DIR=$(pwd)/Runtime/Plugin/iOS
 
 # Install cmake
 brew install cmake
@@ -9,17 +10,6 @@ brew install cmake
 # Download webrtc 
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
-
-# todo(kazuki): build for iOS test
-# Install googletest
-git clone https://github.com/google/googletest.git
-cd googletest
-git checkout 2fe3bd994b3189899d93f1d5a881e725e046fdc2
-mkdir release
-cd release
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-sudo make install
 
 # Build webrtc Unity plugin 
 cd "$SOLUTION_DIR"
@@ -30,10 +20,12 @@ cmake -G Xcode                                 \
   -D CMAKE_XCODE_ATTRIBUTE_ENABLE_BITCODE=YES  \
   .
 
-#xcodebuild -scheme webrtc -configuration Release build
-#xcodebuild -scheme WebRTCPluginTest -configuration Release build
 cmake --build . --config Release --target WebRTCPlugin
+xcodebuild -sdk iphonesimulator -configuration Release
+mv "$WEBRTC_FRAMEWORK_DIR/webrtc.framework" "$WEBRTC_FRAMEWORK_DIR/webrtc-sim.framework"
+xcodebuild -sdk iphoneos -configuration Release
 
-# todo(kazuki): enable test
-# Copy and run the test on the Metal device
-# scp -i ~/.ssh/id_rsa_macmini -o "StrictHostKeyChecking=no" -r "$SOLUTION_DIR/WebRTCPluginTest/Release" bokken@$BOKKEN_DEVICE_IP:~/com.unity.webrtc
+cd "$WEBRTC_FRAMEWORK_DIR"
+lipo -create -o webrtc.framework/webrtc \
+  webrtc.framework/webrtc               \ 
+  webrtc-sim.framework/webrtc
--- Plugin~/cmake/FindFramework.cmake
+++ Plugin~/cmake/FindFramework.cmake
@@ -1,29 +1,16 @@
 if(iOS)
-  find_library(CORE_FOUNDATION Foundation)
-  find_library(AV_FOUNDATION AVFoundation)
-  find_library(CORE_SERVICES CoreServices)
-  find_library(CORE_AUDIO CoreAudio)
-  find_library(CORE_VIDEO CoreVideo)
-  find_library(CORE_MEDIA CoreMedia)
-  find_library(CORE_GRAPHICS CoreGraphics)
-  find_library(AUDIO_TOOLBOX AudioToolbox)
-  find_library(VIDEO_TOOLBOX VideoToolbox)
-  find_library(METAL Metal)
-  find_library(UI_KIT UIKit)
-
   set(FRAMEWORK_LIBS
-    ${CORE_FOUNDATION}
-    ${AV_FOUNDATION}
-    ${APPLICATION_SERVICES}
-    ${CORE_SERVICES}
-    ${CORE_AUDIO}
-    ${CORE_VIDEO}
-    ${CORE_MEDIA}
-    ${CORE_GRAPHICS}
-    ${AUDIO_TOOLBOX}
-    ${VIDEO_TOOLBOX}
-    ${METAL}
-    ${UI_KIT}
+    "-framework Foundation"
+    "-framework AVFoundation"
+    "-framework CoreServices"
+    "-framework CoreAudio"
+    "-framework CoreVideo"    
+    "-framework CoreMedia"
+    "-framework CoreGraphics"
+    "-framework AudioToolbox"
+    "-framework VideoToolbox"
+    "-framework Metal"
+    "-framework UIKit"
   )
 else()
   find_library(CORE_FOUNDATION Foundation)
