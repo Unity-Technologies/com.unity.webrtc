320520f0042d25764b2f5992b17019d9b909621e
kazuki matsumoto <karasusan@gmail.com>
2020-11-25 17:59:37 +0900

    fixed script

--- BuildScripts~/build_plugin_mac.sh
+++ BuildScripts~/build_plugin_mac.sh
@@ -2,6 +2,7 @@
 
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M85/webrtc-mac.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
+export BUNDLE_FILE=$(pwd)/Runtime/Plugins/x86_64/webrtc.bundle
 
 # Install cmake
 brew install cmake
@@ -10,7 +11,10 @@ brew install cmake
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
 
-# Build UnityRenderStreaming Plugin 
+# Remove old bundle file
+rm -r "$BUNDLE_FILE"
+
+# Build UnityRenderStreaming Plugin
 cd "$SOLUTION_DIR"
 cmake -GXcode .
 cmake --build . --config Release --target WebRTCPlugin
