d2acd570989e7daac747373d3a999c576284d171
kazuki matsumoto <karasusan@gmail.com>
2020-11-27 19:09:04 +0900

    refactor CI script

---
+++ BuildScripts~/test_package_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export IDENTITY=~/.ssh/id_rsa_macmini
+
+# render template 
+envsubst '                                    \
+  $SCRIPTING_BACKEND                          \
+  $EXTRA_EDITOR_ARG                           \
+  $PACKAGE_DIR                                \
+  $TEST_RESULT_DIR                            \
+  $EDITOR_VERSION'                            \
+  < BuildScripts~/template/remote.sh.template \
+  > remote.sh
+chmod +x ~/remote.sh
+
+# copy package to remote machine
+scp -i ${IDENTITY} -r ${PACKAGE_DIR} bokken@${BOKKEN_DEVICE_IP}:${PACKAGE_DIR}
+
+# copy shell script to remote machine
+scp -i ${IDENTITY} -r ~/remote.sh bokken@${BOKKEN_DEVICE_IP}:~/remote.sh
+
+ssh -i ${IDENTITY} bokken@${BOKKEN_DEVICE_IP} ~/remote.sh
+scp -i ${IDENTITY} -r bokken@${BOKKEN_DEVICE_IP}:${PACKAGE_DIR}/test-results upm-ci~/test-results
