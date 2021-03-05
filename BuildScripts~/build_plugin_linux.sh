108b7c77ccfbf346e3cfc93bb9019f83545df67d
kazuki <karasusan@gmail.com>
2021-03-05 17:42:16 +0900

    fix bug

--- BuildScripts~/build_plugin_linux.sh
+++ BuildScripts~/build_plugin_linux.sh
@@ -18,7 +18,7 @@ cmake . \
   -D CMAKE_C_COMPILER="clang" \
   -D CMAKE_CXX_COMPILER="clang++" \
   -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
-  -D CMAKE_BUILD_TYPE="Release"
+  -D CMAKE_BUILD_TYPE="Release" \
   -B build
 
 cmake \
