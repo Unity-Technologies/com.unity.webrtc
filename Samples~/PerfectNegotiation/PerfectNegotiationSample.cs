ecd8f4f76bb297833fc009f43234d6464aacc0af
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-04-08 11:53:01 +0900

    implement perfect negotiation process

--- Samples~/PerfectNegotiation/PerfectNegotiationSample.cs
+++ Samples~/PerfectNegotiation/PerfectNegotiationSample.cs
@@ -1,18 +1,15 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.WebRTC;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
-
-delegate void OnMessageHandler(Peer from, Message message);
 
 class PerfectNegotiationSample : MonoBehaviour
 {
 #pragma warning disable 0649
     [SerializeField] private Button callButton;
     [SerializeField] private Button hangUpButton;
-    [SerializeField] private Button addTracksButton;
     [SerializeField] private Camera cam1;
     [SerializeField] private Camera cam2;
     [SerializeField] private RawImage sourceImage1;
@@ -62,68 +59,77 @@ class PerfectNegotiationSample : MonoBehaviour
 [Serializable]
 class Message
 {
-    public RTCSdpType type;
-    public string sdp;
-    public string candidate;
-    public string sdpMid;
-    public int? sdpMLineIndex;
+    public RTCSessionDescription description;
+    public RTCIceCandidate candidate;
+    public bool runCommand;
 }
 
 class Peer : IDisposable
 {
-    private readonly PerfectNegotiationSample parent;
     private readonly bool polite;
+    private PerfectNegotiationSample parent;
     private RTCPeerConnection pc;
 
     private bool makingOffer;
     private bool ignoreOffer;
     private bool srdAnswerPending;
+
     public Peer(PerfectNegotiationSample parent, bool polite)
     {
         this.parent = parent;
         this.polite = polite;
         var config = GetSelectedSdpSemantics();
         pc = new RTCPeerConnection(ref config);
-        pc.OnTrack = e => {
+
+        pc.OnTrack = e =>
+        {
             Debug.Log($"{this} OnTrack");
             // remoteVideo.srcObject = new MediaStream();
             // remoteVideo.srcObject.addTrack(e.Track);
         };
+
         pc.OnIceCandidate = candidate =>
         {
-            var message = new Message
-            {
-                candidate = candidate.Candidate, sdpMid = candidate.SdpMid, sdpMLineIndex = candidate.SdpMLineIndex
-            };
-            parent.PostMessage(this, message);
+            var message = new Message {candidate = candidate};
+            this.parent.PostMessage(this, message);
         };
 
         pc.OnNegotiationNeeded = () =>
         {
-            try
-            {
-                // log('SLD due to negotiationneeded');
-                // assert_equals(pc.signalingState, 'stable', 'negotiationneeded always fires in stable state');
-                // assert_equals(makingOffer, false, 'negotiationneeded not already in progress');
-                makingOffer = true;
-                var op = pc.SetLocalDescription();
-                // assert_equals(pc.signalingState, 'have-local-offer', 'negotiationneeded not racing with onmessage');
-                // assert_equals(pc.localDescription.type, 'offer', 'negotiationneeded SLD worked');
-
-            }
-            catch (Exception)
-            {
-                // fail(e);
-            }
-            finally
-            {
-                makingOffer = false;
-            }
+            this.parent.StartCoroutine(NegotiationProcess());
         };
+    }
+
+    private IEnumerator NegotiationProcess()
+    {
+        Debug.Log("SLD due to negotiationneeded");
+        Assert.AreEqual(pc.SignalingState, RTCSignalingState.Stable,
+            "negotiationneeded always fires in stable state");
+        Assert.AreEqual(makingOffer, false, "negotiationneeded not already in progress");
+
+        makingOffer = true;
+        var op = pc.SetLocalDescription();
+        yield return op;
+
+        if (op.IsError)
+        {
+            makingOffer = false;
+            yield break;
+        }
+
+        Assert.AreEqual(pc.SignalingState, RTCSignalingState.HaveLocalOffer,
+            "negotiationneeded always fires in stable state");
+        Assert.AreEqual(pc.LocalDescription.type, RTCSdpType.Offer, "negotiationneeded SLD worked");
+
+        var offer = new Message {description = pc.LocalDescription};
+        parent.PostMessage(this, offer);
+        makingOffer = false;
     }
 
     public void Dispose()
     {
+        pc.Dispose();
+        parent = null;
     }
 
     public override string ToString()
@@ -142,49 +148,63 @@ class Peer : IDisposable
 
     public void OnMessage(Message message)
     {
-        // try {
-        //     if (description) {
-        //         // If we have a setRemoteDescription() answer operation pending, then
-        //         // we will be "stable" by the time the next setRemoteDescription() is
-        //         // executed, so we count this being stable when deciding whether to
-        //         // ignore the offer.
-        //         const isStable =
-        //         pc.signalingState == 'stable' ||
-        //             (pc.signalingState == 'have-local-offer' && srdAnswerPending);
-        //         ignoreOffer =
-        //             description.type == 'offer' && !polite && (makingOffer || !isStable);
-        //         if (ignoreOffer) {
-        //             log('glare - ignoring offer');
-        //             return;
-        //         }
-        //         srdAnswerPending = description.type == 'answer';
-        //         log(`SRD(${description.type})`);
-        //         await pc.setRemoteDescription(description);
-        //         srdAnswerPending = false;
-        //         if (description.type == 'offer') {
-        //             assert_equals(pc.signalingState, 'have-remote-offer', 'Remote offer');
-        //             assert_equals(pc.remoteDescription.type, 'offer', 'SRD worked');
-        //             log('SLD to get back to stable');
-        //             await pc.setLocalDescription();
-        //             assert_equals(pc.signalingState, 'stable', 'onmessage not racing with negotiationneeded');
-        //             assert_equals(pc.localDescription.type, 'answer', 'onmessage SLD worked');
-        //             send(other, {description: pc.localDescription});
-        //         } else {
-        //             assert_equals(pc.remoteDescription.type, 'answer', 'Answer was set');
-        //             assert_equals(pc.signalingState, 'stable', 'answered');
-        //             pc.dispatchEvent(new Event('negotiated'));
-        //         }
-        //     } else if (candidate) {
-        //         try {
-        //             await pc.addIceCandidate(candidate);
-        //         } catch (e) {
-        //             if (!ignoreOffer) throw e;
-        //         }
-        //     } else if (run) {
-        //         send(window.parent, {[run.id]: await commands[run.cmd]() || 0});
-        //     }
-        // } catch (e) {
-        //     fail(e);
-        // }
+        if (message.runCommand)
+        {
+            //ToDO: run command
+            return;
+        }
+
+        if (message.candidate != null)
+        {
+            if (!pc.AddIceCandidate(message.candidate) && ! ignoreOffer)
+            {
+                throw new ArgumentException("this candidate can not accept.");
+            }
+            return;
+        }
+
+        parent.StartCoroutine(OfferAnswerProcess(message));
+    }
+
+    private IEnumerator OfferAnswerProcess(Message message)
+    {
+        var description = message.description;
+        var isStable =
+            pc.SignalingState == RTCSignalingState.Stable ||
+            (pc.SignalingState == RTCSignalingState.HaveLocalOffer && srdAnswerPending);
+        ignoreOffer =
+            description.type == RTCSdpType.Offer && !polite && (makingOffer || !isStable);
+        if (ignoreOffer)
+        {
+            Debug.Log($"{this} glare - ignoring offer");
+            yield break;
+        }
+
+        srdAnswerPending = description.type == RTCSdpType.Answer;
+        Debug.Log($"SRD {description.type}");
+        var op1 = pc.SetRemoteDescription(ref description);
+        yield return op1;
+
+        srdAnswerPending = false;
+        if (description.type == RTCSdpType.Offer)
+        {
+            Assert.AreEqual(pc.SignalingState, RTCSignalingState.HaveRemoteOffer, "Remote offer");
+            Assert.AreEqual(pc.RemoteDescription.type, RTCSdpType.Offer, "SRD worked");
+            Debug.Log("SLD to get back to stable");
+
+            var op2 = pc.SetLocalDescription();
+            yield return op2;
+
+            Assert.AreEqual(pc.SignalingState, RTCSignalingState.Stable, "onmessage not racing with negotiationneeded");
+            Assert.AreEqual(pc.LocalDescription.type, RTCSdpType.Answer, "onmessage SLD worked");
+
+            var answer = new Message {description = pc.LocalDescription};
+            parent.PostMessage(this, answer);
+        }
+        else
+        {
+            Assert.AreEqual(pc.RemoteDescription.type, RTCSdpType.Answer, "Answer was set");
+            Assert.AreEqual(pc.SignalingState, RTCSignalingState.Stable, "answered");
+        }
     }
 }
