75aac64e9940bcfdeaf52f3283922697bbb7a899
Kazuki Matsumoto <karasusan@gmail.com>
2021-04-22 18:52:55 +0900

    [skip ci] wip

--- BuildScripts~/test_package_mac.sh
+++ BuildScripts~/test_package_mac.sh
@@ -25,6 +25,7 @@ brew install gettext
 envsubst ' \
   $SCRIPTING_BACKEND \
   $EXTRA_EDITOR_ARG \
+  $PLAYER_LOAD_PATH \
   $TEST_PROJECT_DIR \
   $TEST_TARGET \
   $TEST_PLATFORM \
@@ -38,6 +39,8 @@ scp -i ${IDENTITY} -r ${YAMATO_SOURCE_DIR} bokken@${BOKKEN_DEVICE_IP}:~/${PACKAG
 
 # copy shell script to remote machine
 scp -i ${IDENTITY} -r ~/remote.sh bokken@${BOKKEN_DEVICE_IP}:~/remote.sh
+
+scp -i ${IDENTITY} -r build bokken@${BOKKEN_DEVICE_IP}:~/build
 
 # run remote.sh on the remote machine
 ssh -i ${IDENTITY} bokken@${BOKKEN_DEVICE_IP} ~/remote.sh
