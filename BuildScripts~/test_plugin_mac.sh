e164e3b7e6dfb28a8b0d9c3f95b2461c7dd34d5f
kazuki <karasusan@gmail.com>
2021-06-09 14:56:44 +0900

    [skip ci] remve command for installing cmake
    (cherry picked from commit 2b775b3f9159a697cc18a12076041bef28223283)

--- BuildScripts~/test_plugin_mac.sh
+++ BuildScripts~/test_plugin_mac.sh
@@ -3,9 +3,6 @@
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M89/webrtc-mac.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
 
-# Install cmake
-brew install cmake
-
 # Download LibWebRTC 
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip
