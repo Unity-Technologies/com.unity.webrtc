a83f0e030618f77c9c0a85591ef6ad188ae74a48
kazuki <karasusan@gmail.com>
2021-07-05 19:12:22 +0900

    fix

--- BuildScripts~/build_plugin_mac.sh
+++ BuildScripts~/build_plugin_mac.sh
@@ -2,7 +2,7 @@
 
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M89/webrtc-mac.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
-export BUNDLE_FILE=$(pwd)/Runtime/Plugins/x86_64/webrtc.bundle
+export BUNDLE_FILE=$(pwd)/Runtime/Plugins/macOS/webrtc.bundle
 
 # Install cmake
 export HOMEBREW_NO_AUTO_UPDATE=1
