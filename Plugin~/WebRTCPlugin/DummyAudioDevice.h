def1b168731c31a7fa8ba91fbc84ca88a691d71d
Antti Tapaninen <aet@aet.fi>
2022-02-28 20:08:47 -0800

    ﻿remove callback_t

--- Plugin~/WebRTCPlugin/DummyAudioDevice.h
+++ Plugin~/WebRTCPlugin/DummyAudioDevice.h
@@ -193,8 +193,7 @@ namespace webrtc
         std::atomic<bool> playing_ { false };
         std::atomic<bool> recording_ { false };
         mutable std::mutex mutex_;
-        webrtc::AudioTransport* audio_transport_ { nullptr };
-        using callback_t = std::function<void()>;
+        webrtc::AudioTransport* audio_transport_{ nullptr };
         TaskQueueFactory* tackQueueFactory_;
     };
 
