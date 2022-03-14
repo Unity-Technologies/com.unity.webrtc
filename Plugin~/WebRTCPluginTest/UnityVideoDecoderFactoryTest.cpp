554110093090ff852d3937504e13439327528f2b
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2022-03-14 15:21:00 +0900

    test: Add testcase for video encoder/decoder factory (#648)

---
+++ Plugin~/WebRTCPluginTest/UnityVideoDecoderFactoryTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+
+#include "GraphicsDeviceContainer.h"
+#include "UnityVideoDecoderFactory.h"
+
+namespace unity
+{
+namespace webrtc
+{
+    class UnityVideoDecoderFactoryTest : public testing::TestWithParam<UnityGfxRenderer>
+    {
+    public:
+        UnityVideoDecoderFactoryTest()
+            : container_(CreateGraphicsDeviceContainer(GetParam()))
+        {
+        }
+
+    protected:
+        std::unique_ptr<GraphicsDeviceContainer> container_;
+    };
+
+    TEST_P(UnityVideoDecoderFactoryTest, GetSupportedFormats)
+    {
+        auto factory = std::make_unique<UnityVideoDecoderFactory>(container_->device());
+        auto formats = factory->GetSupportedFormats();
+        EXPECT_GT(formats.size(), 0);
+    }
+
+    INSTANTIATE_TEST_SUITE_P(GfxDevice, UnityVideoDecoderFactoryTest, testing::ValuesIn(supportedGfxDevices));
+} // namespace webrtc
+} // namespace unity
---
+++ Plugin~/WebRTCPluginTest/UnityVideoEncoderFactoryTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+
+#include "GraphicsDeviceContainer.h"
+#include "UnityVideoEncoderFactory.h"
+
+namespace unity
+{
+namespace webrtc
+{
+    class UnityVideoEncoderFactoryTest : public testing::TestWithParam<UnityGfxRenderer>
+    {
+    public:
+        UnityVideoEncoderFactoryTest()
+            : container_(CreateGraphicsDeviceContainer(GetParam()))
+        {
+        }
+
+    protected:
+        std::unique_ptr<GraphicsDeviceContainer> container_;
+    };
+
+    TEST_P(UnityVideoEncoderFactoryTest, GetSupportedFormats)
+    {
+        auto factory = std::make_unique<UnityVideoEncoderFactory>(nullptr);
+        auto formats = factory->GetSupportedFormats();
+        EXPECT_GT(formats.size(), 0);
+    }
+
+    INSTANTIATE_TEST_SUITE_P(GfxDevice, UnityVideoEncoderFactoryTest, testing::ValuesIn(supportedGfxDevices));
+} // namespace webrtc
+} // namespace unity
