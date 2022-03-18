b8a2ef24ec7553958c8804b8c12f42c6b4dd3a3a
kazuki <karasusan@gmail.com>
2022-03-18 09:03:35 +0900

    workaround
    (cherry picked from commit 54f701df8445383320e5665c274ce3b334596d2f)
    (cherry picked from commit ca8fe84076f37b17fe216cb4c178e2cfd8d41d3e)

--- Plugin~/WebRTCPluginTest/pch.h
+++ Plugin~/WebRTCPluginTest/pch.h
@@ -12,3 +12,8 @@
 #if defined(LEAK_SANITIZER)
 #include <sanitizer/lsan_interface.h>
 #endif
+
+// workaround:
+// Visual Studio Test Adapter treats Skip type as a error.
+#define GTEST_SKIP_SUCCESS() \
+    return GTEST_MESSAGE_("Skipped", ::testing::TestPartResult::kSuccess)
