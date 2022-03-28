fd6972a2ac0264ea43de98fc87c3373b7808148a
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-03-28 13:02:27 +0900

    remove unused method

--- Tests/Runtime/TestHelper.cs
+++ Tests/Runtime/TestHelper.cs
@@ -5,27 +5,6 @@ namespace Unity.WebRTC.RuntimeTest
 {
     internal class TestHelper
     {
-        public static bool HardwareCodecSupport()
-        {
-            if (!WebRTC.HardwareEncoderSupport())
-                return false;
-            WebRTC.Initialize();
-            var isSupported = CheckVideoCodecCapabilities();
-            WebRTC.Dispose();
-            return isSupported;
-        }
-
-        static bool CheckVideoCodecCapabilities()
-        {
-            var capabilitiesSender = RTCRtpSender.GetCapabilities(TrackKind.Video);
-            if (!capabilitiesSender.codecs.Any())
-                return false;
-            var capabilitiesReceiver = RTCRtpReceiver.GetCapabilities(TrackKind.Video);
-            if (!capabilitiesReceiver.codecs.Any())
-                return false;
-            return true;
-        }
-
         private static readonly string[] excludeCodecMimeType = {"video/red", "video/ulpfec", "video/rtx"};
 
         public static bool CheckVideoSendRecvCodecSupport()
