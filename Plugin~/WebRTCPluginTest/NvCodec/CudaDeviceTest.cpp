4991afa8133ffeedf4b1901e50c40cbc50c4cb14
kazuki <karasusan@gmail.com>
2022-04-04 08:51:33 +0900

    revert

--- Plugin~/WebRTCPluginTest/NvCodec/CudaDeviceTest.cpp
+++ Plugin~/WebRTCPluginTest/NvCodec/CudaDeviceTest.cpp
@@ -33,27 +33,11 @@ namespace webrtc
 
         std::unique_ptr<GraphicsDeviceContainer> container_;
         IGraphicsDevice* device_;
-        const uint32_t kWidth = 256;
-        const uint32_t kHeight = 256;
-        const UnityRenderingExtTextureFormat kFormat = kUnityRenderingExtFormatR8G8B8A8_SRGB;
     };
 
     TEST_P(CudaDeviceTest, GetCUcontext) { EXPECT_NE(device_->GetCUcontext(), nullptr); }
 
     TEST_P(CudaDeviceTest, IsCudaSupport) { EXPECT_TRUE(device_->IsCudaSupport()); }
-
-    TEST_P(CudaDeviceTest, Resize) 
-    {
-        const uint32_t kWidth2 = kWidth * 2;
-        const uint32_t kHeight2 = kHeight * 2;
-
-        std::unique_ptr<ITexture2D> texture(device_->CreateDefaultTextureV(kWidth2, kHeight2, kFormat));
-        std::unique_ptr<GpuMemoryBufferHandle> handle = device_->Map(texture.get());
-
-        GpuMemoryBufferCudaHandle* cudaHandle = static_cast<GpuMemoryBufferCudaHandle*>(handle.get());
-
-        //std::unique_ptr<GpuMemoryBufferCudaHandle> resized(device_->Resize(*cudaHandle, Size(kWidth, kHeight)));
-    }
 
     INSTANTIATE_TEST_SUITE_P(GfxDevice, CudaDeviceTest, testing::ValuesIn(supportedGfxDevices));
 
