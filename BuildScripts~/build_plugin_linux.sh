f46dfe7d1dbca7dc80d6d07688f7cb80712331b6
kazuki <karasusan@gmail.com>
2022-10-17 23:51:23 +0900

    remove libc++.so.1 dependency

--- BuildScripts~/build_plugin_linux.sh
+++ BuildScripts~/build_plugin_linux.sh
@@ -2,6 +2,7 @@
 
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M92/webrtc-linux.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
+export LIBCXX_BUILD_DIR=$(pwd)/llvm-project/build
 
 source ~/.profile
 
@@ -15,13 +16,23 @@ sudo apt install -y libglfw3-dev
 # Install glad2
 pip3 install git+https://github.com/dav1dde/glad.git@glad2#egg=glad
 
+# Make libc++ static library
+apt install ninja-build
+git clone --depth 1 --branch release/13.x https://github.com/llvm/llvm-project.git
+pushd llvm-project
+mkdir build
+cmake -G Ninja -S runtimes -B build -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
+ninja -C build cxx cxxabi
+popd
+
 # Build UnityRenderStreaming Plugin 
 cd "$SOLUTION_DIR"
 cmake . \
   -D CMAKE_C_COMPILER="clang-10" \
   -D CMAKE_CXX_COMPILER="clang++-10" \
-  -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
   -D CMAKE_BUILD_TYPE="Release" \
+  -D USE_CUSTOM_LIBCXX_STATIC=ON \
+  -D CUSTOM_LIBCXX_DIR="$LIBCXX_BUILD_DIR" \
   -B build
 
 cmake \
