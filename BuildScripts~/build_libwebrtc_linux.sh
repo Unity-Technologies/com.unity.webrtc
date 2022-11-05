cd59697f1fbe985b879992722fa8704828f70380
kazuki <karasusan@gmail.com>
2022-11-05 14:50:29 +0900

    fix

--- BuildScripts~/build_libwebrtc_linux.sh
+++ BuildScripts~/build_libwebrtc_linux.sh
@@ -43,8 +43,7 @@ do
       enable_iterator_debugging=false \
       is_component_build=false \
       use_rtti=true \
-      rtc_use_x11=false \
-      libcxx_abi_unstable=false"
+      rtc_use_x11=false"
 
     if [ $is_debug = "true" ]; then
       args="${args} is_asan=true is_lsan=true";
