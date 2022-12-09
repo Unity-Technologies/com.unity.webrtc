cda26317544d75af3ee949a9f2c0f3437d10a7ee
kazuki <karasusan@gmail.com>
2022-12-09 22:28:18 +0900

    fix

--- BuildScripts~/build_testrunner_linux.sh
+++ BuildScripts~/build_testrunner_linux.sh
@@ -3,13 +3,6 @@
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M107/webrtc-linux.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
 
-# See False positives
-# https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow
-export ASAN_OPTIONS=protect_shadow_gap=0:detect_leaks=1:detect_container_overflow=0
-export LSAN_OPTIONS=suppressions=$(pwd)/Plugin~/tools/sanitizer/lsan_suppressions.txt
-
-source ~/.profile
-
 # Download LibWebRTC 
 curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
