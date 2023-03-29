ffd07dabdd606207177692396961c17432f13529
Takashi KANNAN <takashi.kannan@unity3d.com>
2023-03-29 14:34:57 +0900

    downgrade jdk 17 to 11 to 8

--- BuildScripts~/build_libwebrtc_android.sh
+++ BuildScripts~/build_libwebrtc_android.sh
@@ -37,6 +37,7 @@ patch -N "src/build/config/BUILD.gn" < "$COMMAND_DIR/patches/add_deps_libunwind.
 # downgrade JDK11 to JDK8 because Unity supports OpenJDK version 1.8.
 # https://docs.unity3d.com/Manual/android-sdksetup.html
 pushd "src/build"
+git apply "$COMMAND_DIR/patches/downgrade_JDK_17to11.patch"
 git apply "$COMMAND_DIR/patches/downgrade_JDK.patch"
 popd
 
