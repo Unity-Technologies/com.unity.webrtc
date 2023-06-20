9e7acd8b4a4985b5275582e4794ca8d6aac5c74b
kazuki <karasusan@gmail.com>
2023-06-20 17:46:04 +0900

    format

--- Plugin~/WebRTCPlugin/GraphicsDevice/D3D12/D3D12Texture2D.cpp
+++ Plugin~/WebRTCPlugin/GraphicsDevice/D3D12/D3D12Texture2D.cpp
@@ -10,8 +10,7 @@ namespace unity
 {
 namespace webrtc
 {
-    D3D12Texture2D::D3D12Texture2D(
-        uint32_t w, uint32_t h, ID3D12Resource* nativeTex, HANDLE handle)
+    D3D12Texture2D::D3D12Texture2D(uint32_t w, uint32_t h, ID3D12Resource* nativeTex, HANDLE handle)
         : ITexture2D(w, h)
         , m_nativeTexture(nativeTex)
         , m_sharedHandle(handle)
@@ -20,7 +19,8 @@ namespace webrtc
     {
     }
 
-    D3D12Texture2D::D3D12Texture2D(uint32_t w, uint32_t h, ID3D12Resource* nativeTex, const D3D12ResourceFootprint& footprint)
+    D3D12Texture2D::D3D12Texture2D(
+        uint32_t w, uint32_t h, ID3D12Resource* nativeTex, const D3D12ResourceFootprint& footprint)
         : ITexture2D(w, h)
         , m_nativeTexture(nativeTex)
         , m_sharedHandle(nullptr)
