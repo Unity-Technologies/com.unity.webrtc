cf087112e46f5c8d77f0725218b6a1d5114334bd
kazuki <karasusan@gmail.com>
2023-07-12 10:58:01 +0900

    fix

--- Samples~/VideoReceive/VideoReceiveSample.cs
+++ Samples~/VideoReceive/VideoReceiveSample.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
 
 namespace Unity.WebRTC.Samples
 {
