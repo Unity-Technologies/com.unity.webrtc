3dddb5c3b6d9bee94708f54c27a6a75f41715ede
kazuki <karasusan@gmail.com>
2023-10-06 11:57:21 +0900

    format

--- Plugin~/WebRTCPlugin/WebRTCPlugin.cpp
+++ Plugin~/WebRTCPlugin/WebRTCPlugin.cpp
@@ -1390,11 +1390,9 @@ extern "C"
             return nullptr;
 
         std::vector<::RtpSource> result;
-        std::transform(
-            sources.begin(),
-            sources.end(),
-            std::back_inserter(result),
-            [](webrtc::RtpSource source) { return source; });
+        std::transform(sources.begin(), sources.end(), std::back_inserter(result), [](webrtc::RtpSource source) {
+            return source;
+        });
         return ConvertArray(result, length);
     }
 
