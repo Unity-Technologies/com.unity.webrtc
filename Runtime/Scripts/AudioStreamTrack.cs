79013018d30a1ab5356105e6c4b7280fbccb7bdd
Masayuki Iwai <miyabi@mybdesign.com>
2024-12-07 08:41:13 +0900

    Update Runtime/Scripts/AudioStreamTrack.cs
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>

--- Runtime/Scripts/AudioStreamTrack.cs
+++ Runtime/Scripts/AudioStreamTrack.cs
@@ -440,7 +440,7 @@ namespace Unity.WebRTC
 #endif
 
         /// <summary>
-        ///     Event to be fired when received new audio data.
+        ///     Event to be fired when new audio data is received.
         /// </summary>
         /// <remarks>
         ///     `onReceived` event is fired when received new audio data.
