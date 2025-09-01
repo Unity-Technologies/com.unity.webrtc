ecf2d95fdf2f2811cab47df8f79c55e51609b03c
Kazuki Matsumoto <kazuki@unity3d.com>
2025-09-01 15:57:11 +0900

    add Wrench

---
+++ Tools/Wrench/Settings/TEMPLATESettings.cs
@@ -0,0 +1,27 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace TEMPLATE.Cookbook.Settings;
+
+public class TEMPLATESettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"PACKAGES_ROOTS"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        //"PACKAGES_TO_RELEASE"
+    };
+
+    public TEMPLATESettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
