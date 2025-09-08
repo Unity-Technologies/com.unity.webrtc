d6345ffa8744083ca77746007e793dff2a1fa9c4
Kazuki Matsumoto <kazuki@unity3d.com>
2025-09-08 20:48:11 +0900

    fix

--- Tests/Runtime/MediaStreamTrackTest.cs
+++ Tests/Runtime/MediaStreamTrackTest.cs
@@ -91,9 +91,11 @@ namespace Unity.WebRTC.RuntimeTest
             var height = 256;
             var format = RenderTextureFormat.R8;
             var rt = new RenderTexture(width, height, 0, format);
-            var created = rt.Create();
-            if (!created)
+            if (rt.format != format)
+            {
                 Assert.Ignore("RenderTextureFormat.R8 texture format is not supported on this platform.");
+            }
+            rt.Create();
 
             Assert.That(() => { new VideoStreamTrack(rt); }, Throws.TypeOf<ArgumentException>());
             Object.DestroyImmediate(rt);
